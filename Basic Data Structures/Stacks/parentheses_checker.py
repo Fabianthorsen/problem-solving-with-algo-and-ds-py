c7fb0cf88f2183749d2688d3132045320cc3d06e
Fabian Thorsen <thorsen.fabian@gmail.com>
2022-01-15 14:48:33 +0100

    Extend to handle braces and square braces as well

--- Basic Data Structures/Stacks/parentheses_checker.py
+++ Basic Data Structures/Stacks/parentheses_checker.py
@@ -5,23 +5,29 @@ def par_checker(symbol_string: str) -> bool:
     s = Stack()
     balanced = True
     index = 0
+
     while index < len(symbol_string) and balanced:
         # Set current char as symbol
         symbol = symbol_string[index]
 
-        # If symbol is a left parenthesis
-        if symbol == "(":
+        # If symbol is a left parenthesis, left square bracket or left bracket
+        if symbol in "([{":
             # Push to stack
             s.push(symbol)
         else:
 
-            # If not a left parenthesis and stack is empty
+            # If not a left, stack is empty
             if s.is_empty():
                 # The string isn't balanced
                 balanced = False
             else:
                 # If not empty then we pop one item from stack
-                s.pop()
+                top = s.pop()
+
+                # Check if the top of stack, matches the current: ( or [ or {
+                if not matches(top, symbol):
+                    # Un-balanced if no match
+                    balanced = False
 
         # Increment index once, and re-run
         index += 1
@@ -35,5 +41,11 @@ def par_checker(symbol_string: str) -> bool:
         return False
 
 
-print(par_checker("((()))"))  # Balanced
-print(par_checker("(()"))  # Un-balanced
+def matches(open: str, close: str) -> bool:
+    opens = "([{"
+    closes = ")]}"
+    return opens.index(open) == closes.index(close)
+
+
+print(par_checker("{{([][])}()}"))  # Balanced
+print(par_checker("[{()]"))  # Un-balanced
